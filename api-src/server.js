041735a38694c16e998c1ec3d43b5331cd92ec97
J.R. Ruggiero <fleury14@comcast.net>
2018-02-26 14:42:26 -0800

    initial commit for angular package and api files

---
+++ api-src/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const app = express();
+const mongoose = require('mongoose');
+const config = require('./config');
+const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 8100;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// CONNECT TO DATABASE
+
+// API ROUTING
+// middleware to use for all requests
+app.use(function (req, res, next) {
+  console.log('using router');
+  next();
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+// Test route
+router.get('/test', function (req, res) {
+  res.json({ message: 'api really works!' });
+});
+
+
+// Prefix routes with /api
+app.use('/api', router);
+
+
+
+// START THE SERVER
+app.listen(port, () => {
+  console.log('Listening on port ' + port);
+});
