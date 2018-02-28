4cf5325e1c50fea7ea18fe46ecc9505591c19a73
J.R. Ruggiero <fleury14@comcast.net>
2018-02-27 16:48:36 -0800

    fixed compile error: dont need no config

--- api-src/server.js
+++ api-src/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const app = express();
 const mongoose = require('mongoose');
-const config = require('./config');
 const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 3000;
